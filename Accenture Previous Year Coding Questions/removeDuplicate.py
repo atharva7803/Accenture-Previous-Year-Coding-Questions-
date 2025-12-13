134356ec9f80c873010be5fe81cdf0b4c0437eb5
mohammedkaif777 <113031144+mohammedkaif777@users.noreply.github.com>
2025-12-13 13:38:37 +0530

    Update removeDuplicate.py

--- Accenture Previous Year Coding Questions/removeDuplicate.py
+++ Accenture Previous Year Coding Questions/removeDuplicate.py
@@ -18,4 +18,26 @@ def main():
     
 
 if __name__ == "__main__":
+
     main()
+
+//Java Code
+# import java.util.*;
+
+# class Main {
+#   public static void main(String[] args) {
+#     int[] arr = {1, 2, 2, 3, 4, 4, 5};
+#     int[] ans = duplicateElem(arr);
+#     System.out.println(Arrays.toString(ans));
+#   }
+
+#   public static int[] duplicateElem(int[] arr) {
+#     HashSet<Integer> set = new HashSet<>();
+#     for (int i : arr) set.add(i);
+
+#     int[] res = new int[set.size()];
+#     int idx = 0;
+#     for (int i : set) res[idx++] = i;
+#     return res;
+#   }
+# }
